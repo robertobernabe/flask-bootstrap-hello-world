9590257591eeca57147d43807fafdd96b09da091
Florian Schaeffeler <florian.schaeffeler@avira.com>
2019-08-20 18:26:40 +0200

    basic flask app serving bootstrap 4

--- hello_world/routes.py
+++ hello_world/routes.py
@@ -1,7 +1,14 @@
 from hello_world import app
+from flask import render_template
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World!"
+    say = {"msg": "Hello World. Hello Bootrap"}
+    return render_template("index.html", title="Home", say=say)
+
+
+@app.route("/cover")
+def cover():
+    return render_template("cover.html")
